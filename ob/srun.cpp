633b82fcca5d2ab831a1a5bf19e2ef934962b647
abar-workout <154221849+abar-workout@users.noreply.github.com>
2023-12-22 18:41:14 +0300

    Delete ob directory

--- ob/srun.cpp
+++
@@ -1,9 +0,0 @@
-int main() {
-    // Запуск программы p1.cpp
-    system("g++ src/s1.cpp -o s1 && ./s1");
-
-    // Запуск программы p2.cpp
-    system("g++ src/s2.cpp -o s2 && ./s2");
-
-    return 0;
-}
